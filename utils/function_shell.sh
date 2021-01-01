0f2211b85b105c130484faae98cd93da336dc0c8
YunSpringWang <719344350@qq.com>
2021-01-01 18:19:11 +0800

    add extract shell

---
+++ utils/function_shell.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function extract () {
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xvjf $1    ;;
+      *.tar.gz)    tar xvzf $1    ;;
+      *.tar.xz)    tar Jxvf $1    ;;
+      *.bz2)       bunzip2 $1     ;;
+      *.rar)       rar x $1       ;;
+      *.gz)        gunzip $1      ;;
+      *.tar)       tar xvf $1     ;;
+      *.tbz2)      tar xvjf $1    ;;
+      *.tgz)       tar xvzf $1    ;;
+      *.zip)       unzip -d `echo $1 | sed 's/\(.*\)\.zip/\1/'` $1;;
+      *.Z)         uncompress $1  ;;
+      *.7z)        7z x $1        ;;
+      *)           echo "don't know how to extract '$1'" ;;
+    esac
+  else
+    echo "'$1' is not a valid file!"
+  fi
+}
+
+function ddext4() {
+sudo dd if=/dev/null of="$1" count=1 bs=8M conv=notrunc
+sudo fdisk "$1"
+sudo mkfs.ext4 "$1"
+}
+function q () {
+	exit 1
+}
